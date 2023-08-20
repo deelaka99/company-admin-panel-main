1a4c97d4e995307cc2792e1c4ea83e0c1284cb3f
deelaka99 <123wejith@gmail.com>
2023-08-20 23:33:13 +0530

    added some adjusment to the table of manage

--- src/components/management/Manage.jsx
+++ src/components/management/Manage.jsx
@@ -3,178 +3,46 @@ import React from "react";
 function Manage() {
   return (
     <div className="flex flex-col w-full h-full">
-      
       <div className="h-full w-full flex items-center justify-center">
-        <div className=" h-5/6 w-11/12">
-          <div className="h-full w-full flex items-center justify-center overflow-x-auto drop-shadow-2xl rounded-md overflow-y-auto">
-            <table className="bg-ternary-blue dark:bg-dark-ternary p-3 h-full w-full ">
-              <thead>
-                <tr className="bg-secondary-blue dark:bg-dark-secondary">
-                  <th className="p-1">Lab ID</th>
-                  <th className="p-1">Name</th>
-                  <th className="p-1">Address</th>
-                  <th className="p-1">Contact</th>
-                  <th className="p-1">Status</th>
-                  <th className="p-1">Action</th>
+        <div className="h-5/6 w-11/12 overflow-hidden">
+          <div class="relative w-full h-full overflow-y-auto overflow-x-auto  shadow-md rounded-lg">
+            <table class="w-full text-sm text-center  text-primary-blue dark:text-white">
+              <thead class="text-xs text-white  uppercase bg-secondary-blue dark:bg-black dark:text-dark-ternary">
+                <tr>
+                  <th scope="col" class="px-6 py-3">
+                    Lab Name
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Address
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Telephone
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Email
+                  </th>
+                  <th scope="col" class="px-4 py-3">
+                    Statues
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Action
+                  </th>
                 </tr>
               </thead>
-              <tbody>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
+              <tbody className="bg-ternary-blue dark:bg-dark-ternary overflow-y-auto">
+                <tr class="border-b">
+                  <td class="px-6 py-4">Durdans</td>
+                  <td class="px-6 py-4">nugegoda</td>
+                  <td class="px-6 py-4">0112831071</td>
+                  <td class="px-6 py-4">123wejith@gmail.com</td>
+                  <td class="px-4 py-4">ACTIVE</td>
+                  <td class="px-6 py-4 space-x-2">
+                    <button className="bg-blue text-white p-1 rounded shadow-lg hover:opacity-80">Edit</button>
+                    <button className="bg-yellow text-black p-1 rounded shadow-lg hover:opacity-80">Block</button>
+                    <button className="bg-red text-white p-1 rounded shadow-lg hover:opacity-80">Remove</button>
                   </td>
                 </tr>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="text-center">
-                  <td>001</td>
-                  <td>A</td>
-                  <td>Galle</td>
-                  <td>07713377130</td>
-                  <td>Active</td>
-                  <td>
-                    <div className="flex items-center justify-center">
-                      <div className="w-1/3">
-                        <button className="bg-blue text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Update
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-red text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Remove
-                        </button>
-                      </div>
-                      <div className="w-1/3">
-                        <button className="bg-yellow text-center p-1 text-white rounded-md hover:opacity-70 h-full">
-                          Block
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
+                
               </tbody>
             </table>
           </div>
