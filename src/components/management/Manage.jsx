a3d5691b9854c28bede7e489709a7ee9f619595c
deelaka99 <123wejith@gmail.com>
2023-08-22 22:59:05 +0530

    added input values from firebase to edit modal

--- src/components/management/Manage.jsx
+++ src/components/management/Manage.jsx
@@ -1,9 +1,20 @@
 import { React, useState, useEffect } from "react";
 import { db } from "../../firebase";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, update } from "firebase/database";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 function Manage() {
+  const [showEditModal, setShowEditModal] = useState(false);
   const [labsData, setLabsData] = useState([]); // State to store retrieved data
+  // const [editLabData, setEditLabData] = useState({}); // State to store lab data for editing
+
+  const [selectedLab, setSelectedLab] = useState({
+    LabName: "",
+    address: "",
+    telephone: "",
+    email: "",
+  });
 
   // useEffect hook to fetch data from Firebase
   useEffect(() => {
@@ -17,6 +28,23 @@ function Manage() {
       setLabsData(labsData);
     });
   }, []);
+
+  // Function to update lab data in Firebase
+  // const updateLabData = async () => {
+  //   try {
+  //     const labRef = ref(db, `labs/${editLabData.uuid}`); // Assuming "uuid" is the unique identifier
+  //     await update(labRef, {
+  //       LabName: editLabData.LabName,
+  //       address: editLabData.address,
+  //       telephone: editLabData.telephone,
+  //       email: editLabData.email
+  //     });
+  //     setShowEditModal(false);
+  //   } catch (error) {
+  //     //handle error
+  //     console.error("Error updating lab data:", error);
+  //   }
+  // };
 
   return (
     <div className="flex flex-col w-full h-full">
@@ -55,7 +83,14 @@ function Manage() {
                     <td className="px-6 py-4">{lab.email}</td>
                     <td className="px-6 py-4">Active</td>
                     <td className="px-6 py-4 space-x-2 flex">
-                      <button className="bg-blue text-white p-1 rounded shadow-lg hover:opacity-80">
+                      <button
+                        className="bg-blue text-white p-1 rounded shadow-lg hover:opacity-80"
+                        onClick={() => {
+                          setSelectedLab(lab);
+                          setShowEditModal(true);
+                          setEditLabData({ ...lab }); 
+                        }}
+                      >
                         Edit
                       </button>
                       <button className="bg-yellow text-black p-1 rounded shadow-lg hover:opacity-80">
@@ -72,6 +107,124 @@ function Manage() {
           </div>
         </div>
       </div>
+
+      {/**Edit lab modal */}
+      {showEditModal ? (
+        <div>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none text-primary-blue dark:text-white">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="p-2 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-ternary-blue dark:bg-dark-secondary dark:border-2 dark:border-dark-ternary outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-1 rounded-t">
+                  <h3 className="text-xl font-semibold">Edit Lab</h3>
+                  <button
+                    className=" ml-auto  border-0 text-primary-blue font-semibold active:text-black"
+                    onClick={() => setShowEditModal(false)}
+                  >
+                    <span className=" text-primary-blue drop-shadow-lg shadow-black h-6 w-6 text-3xl block dark:text-white flex items-center justify-center">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-2 flex flex-col">
+                  <form>
+                    <div className="h-1/5 w-full flex flex-col">
+                      <div className="">
+                        <p className="font-semibold">Lab Name :</p>
+                      </div>
+                      <div className="pt-2 pb-2">
+                        <input
+                          type="text"
+                          placeholder="Enter new Lab name"
+                          className="rounded-full p-2 h-3/5 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
+                          value={selectedLab.LabName}
+                          onChange={(e) =>
+                            setSelectedLab({
+                              ...selectedLab,
+                              LabName: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="h-1/5 w-full flex flex-col">
+                      <div>
+                        <p className="font-semibold">Address :</p>
+                      </div>
+                      <div className="pt-2 pb-2">
+                        <input
+                          type="text"
+                          placeholder="Enter new Address"
+                          className="rounded-full p-2 h-3/5 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
+                          value={selectedLab.address}
+                          onChange={(e) =>
+                            setSelectedLab({
+                              ...selectedLab,
+                              address: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="h-1/5 w-full flex flex-col">
+                      <div>
+                        <p className="font-semibold">Telephone :</p>
+                      </div>
+                      <div className="pt-2 pb-2">
+                        <input
+                          type="text"
+                          placeholder="Enter new Telephone"
+                          className="rounded-full p-2 h-3/5 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
+                          value={selectedLab.telephone}
+                          onChange={(e) =>
+                            setSelectedLab({
+                              ...selectedLab,
+                              telephone: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="h-1/5 w-full flex flex-col">
+                      <div>
+                        <p className="font-semibold">Email :</p>
+                      </div>
+                      <div className="pt-2 pb-2">
+                        <input
+                          type="email"
+                          placeholder="Enter new E-mail"
+                          className="rounded-full p-2 h-3/5 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
+                          value={selectedLab.email}
+                          onChange={(e) =>
+                            setSelectedLab({
+                              ...selectedLab,
+                              email: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-center p-1 rounded-b">
+                  <button
+                    className="bg-primary-blue text-white active:bg-black font-bold uppercase text-md px-3 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 dark:bg-dark-primary"
+                    type="button"
+                    onClick={() => setShowEditModal(false)}
+                  >
+                    <FontAwesomeIcon icon={faFloppyDisk} />
+                    &nbsp; Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg opacity-50 fixed inset-0 z-40 bg-black"></div>
+        </div>
+      ) : null}
     </div>
   );
 }
