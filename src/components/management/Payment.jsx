5f18d32f60304306e73eeb3abab48f13c19b43dd
deelaka99 <123wejith@gmail.com>
2023-08-28 13:17:16 +0530

    change pw is completed

--- src/components/management/Payment.jsx
+++ src/components/management/Payment.jsx
@@ -7,32 +7,8 @@ function Payment() {
     <div className="flex flex-col w-full h-full">
       <div className="h-full w-full flex items-center justify-center">
         <div className=" h-5/6 w-11/12">
-          <div className="h-full w-full flex flex-col overflow-hidden">
-            <div className="w-full h-2/4 flex items-center justify-center">
-              <div className="w-full h-5/6 rounded-md bg-primary-blue border-2 border-ternary-blue dark:bg-dark-primary dark:border-dark-ternary">
-                <div className="w-full h-1/12 text-center text-white p-2">
-                  <h1 className="text-lg font-medium">Make Payment</h1>
-                  <p className="text-sm font-inter">Choose payment method</p>
-                </div>
-                <div className="w-full h-11/12 p-2 flex items-center justify-center">
-                  <div className="flex items-center justify-center pr-1">
-                    <button className="bg-ternary-blue rounded-md flex items-center justify-center hover:drop-shadow-lg active:bg-white dark:bg-dark-secondary dark:drop-shadow-lg dark:active:bg-dark-ternary">
-                      <img src={visa} alt="visa" className="h-1/5 w-1/5" />
-                    </button>
-                  </div>
-                  <div className="flex items-center justify-center pl-1">
-                    <button className="bg-ternary-blue rounded-md flex items-center justify-center hover:drop-shadow-lg active:bg-white dark:bg-dark-secondary dark:drop-shadow-lg dark:active:bg-dark-ternary">
-                      <img
-                        src={mastercard}
-                        alt="mastercard"
-                        className="h-1/5 w-1/5"
-                      />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full h-2/4 pt-3 flex items-center justify-center overflow-y-auto">
+          <div className="h-full w-full overflow-hidden">
+            <div className="w-full h-full pt-3 flex items-center justify-center overflow-y-auto">
               <div className="w-full  rounded-md">
                 <table className="bg-ternary-blue dark:bg-dark-ternary p-3 h-full w-full ">
                   <thead className="">
--- src/components/settings/Security.jsx
+++ src/components/settings/Security.jsx
@@ -7,10 +7,61 @@ import {
   faCaretRight,
 } from "@fortawesome/free-solid-svg-icons";
 import Toggle from "./Toggle";
+import { auth } from "../../firebase";
+import {
+  updatePassword,
+  reauthenticateWithCredential,
+  EmailAuthProvider,
+} from "firebase/auth";
+import NotificationModal from "../../components/Modal/NotificationModal";
 
 function Security() {
+  const [loading, setLoading] = useState(false);
   const [showChangePasswordModal, setChangePasswordShowModal] = useState(false);
   const [showTFVModal, setTFVShowModal] = useState(false);
+  const [showCurrentPwNotMatchModal, setShowCurrentPwNotMatchModal] = useState(false);
+  const [showPwUpdateSuccessModal, setShowPwUpdateSuccessModal] =
+    useState(false);
+  const [showPwUpdateUnsuccessModal, setShowPwUpdateUnsuccessModal] =
+    useState(false);
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleChangePasswordClick = async () => {
+    try {
+      setLoading(true); // Set loading state to true
+      // Reauthenticate the user with their current password
+      const user = auth.currentUser;
+      const credential = EmailAuthProvider.credential(user.email, oldPassword);
+      try {
+        await reauthenticateWithCredential(user, credential);
+      } catch (error) {
+        console.error("Error updating password:", error);
+        setShowCurrentPwNotMatchModal(true);
+      }
+      // Update the user's password
+      if (newPassword === confirmPassword) {
+        await updatePassword(user, newPassword);
+        // Password updated successfully
+        console.log("Password updated successfully");
+        setChangePasswordShowModal(false);
+        setShowPwUpdateSuccessModal(true);
+      } else {
+        console.log(
+          "Error updating password: confirm and new passwrords are not matching!"
+        );
+        setChangePasswordShowModal(false);
+        setShowPwUpdateUnsuccessModal(true);
+      }
+    } catch (error) {
+      console.error("Error updating password:", error);
+      setPasswordsMatch(false);
+    } finally {
+      setLoading(false); // Set loading state to false
+    }
+  };
+
   return (
     <div className="w-11/12 h-5/6 p-2 text-primary-blue font-inter font-md flex flex-col dark:text-white">
       <div className="h-1/5 w-full"></div>
@@ -85,6 +136,8 @@ function Security() {
                         <input
                           type="password"
                           placeholder="Current password"
+                          value={oldPassword}
+                          onChange={(e) => setOldPassword(e.target.value)}
                           className="rounded-full p-2 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
                         />
                       </div>
@@ -97,6 +150,8 @@ function Security() {
                         <input
                           type="password"
                           placeholder="Enter your new password"
+                          value={newPassword}
+                          onChange={(e) => setNewPassword(e.target.value)}
                           className="rounded-full p-2 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
                         />
                       </div>
@@ -109,6 +164,8 @@ function Security() {
                         <input
                           type="password"
                           placeholder="Confirm your new password"
+                          value={confirmPassword}
+                          onChange={(e) => setConfirmPassword(e.target.value)}
                           className="rounded-full p-2 w-full bg-white border-primary-blue border-2 text-center font-semibold dark:border-dark-ternary dark:bg-dark-ternary active:bg-secondary-blue dark:active:bg-dark-secondary"
                         />
                       </div>
@@ -120,7 +177,10 @@ function Security() {
                   <button
                     className="bg-primary-blue text-white active:bg-black font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 dark:bg-dark-primary"
                     type="button"
-                    onClick={() => setChangePasswordShowModal(false)}
+                    onClick={() => {
+                      handleChangePasswordClick();
+                      setChangePasswordShowModal(false);
+                    }}
                   >
                     Change Password
                   </button>
@@ -183,6 +243,53 @@ function Security() {
           </div>
           <div className="rounded-lg opacity-50 fixed inset-0 z-40 bg-black"></div>
         </div>
+      ) : null}
+      {loading ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none text-primary-blue dark:text-white">
+            <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-b-4 border-primary-blue dark:border-white"></div>
+          </div>
+          <div className="opacity-50 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+
+      {/**Pw update Success */}
+      {showPwUpdateSuccessModal ? (
+        <NotificationModal
+          show={showPwUpdateSuccessModal}
+          onClose={() => {
+            setShowPwUpdateSuccessModal(false);
+          }}
+          title="Notification"
+          body=" Password Updated! 🤗"
+          color="green"
+        />
+      ) : null}
+
+      {/**Pw update Unsuccess */}
+      {showPwUpdateUnsuccessModal ? (
+        <NotificationModal
+          show={showPwUpdateUnsuccessModal}
+          onClose={() => {
+            setShowPwUpdateUnsuccessModal(false);
+          }}
+          title="Notification"
+          body=" Not matching New and Confirm Passwords! 😥"
+          color="red"
+        />
+      ) : null}
+
+      {/**Current pw not matching */}
+      {showCurrentPwNotMatchModal ? (
+        <NotificationModal
+          show={showCurrentPwNotMatchModal}
+          onClose={() => {
+            setShowCurrentPwNotMatchModal(false);
+          }}
+          title="Notification"
+          body=" Wrong Current Password! 😥"
+          color="red"
+        />
       ) : null}
     </div>
   );
