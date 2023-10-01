059275eee96c88a245eda60a60bf208864a83314
deelaka99 <123wejith@gmail.com>
2023-10-01 20:09:44 +0530

    modified navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faMagnifyingGlass,
-  faCaretDown,
-  faCaretUp,
-} from "@fortawesome/free-solid-svg-icons";
-
-import { auth } from "../../firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { logout } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { ref, onValue } from "firebase/database";
 import Switcher from "../darkMode/Switcher";
 
 const Navbar = () => {
-  const [user] = useAuthState(auth);
-  const [isOpen, setIsOpen] = useState(false);
+  const user = auth.currentUser;
+  const [timeOfDay, setTimeOfDay] = useState("");
+  const [adminData, setAdminData] = useState([]);
+
+  useEffect(() => {
+    //show greetings according to time
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      setTimeOfDay("Good morning!");
+    } else if (currentHour >= 12 && currentHour < 17) {
+      setTimeOfDay("Good afternoon!");
+    } else {
+      setTimeOfDay("Good evening!");
+    }
+
+    // Fetch admin data from Realtime Database
+    if (user) {
+      const userRef = ref(db, `admins/${user.uid}`);
+      onValue(userRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const adminDetails = snapshot.val();
+          setAdminData(adminDetails);
+        }
+      });
+    }
+  }, []);
 
   return (
     <>
       {/**Search bar */}
-      <div className="h-full w-2/5 flex justify-center items-center ">
-        <div className="bg-ternary-blue h-1/2 w-2/3 rounded-3xl opacity-100 flex drop-shadow-xl dark:bg-dark-ternary">
-          <div className="h-full w-10/12 rounded-3xl flex justify-center items-center ">
-            Search...
-          </div>
-          <div className="bg-bermuda  h-full w-2/12 rounded-3xl flex justify-center items-center drop-shadow-xl hover:text-silver dark:bg-dark-primary dark:text-dark-ternary dark:hover:text-silver">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </div>
-        </div>
-      </div>
+      <div className="h-full w-2/5 flex justify-center items-center "></div>
 
       {/**space */}
       <div className="h-full w-1/5"></div>
@@ -42,44 +50,25 @@ const Navbar = () => {
           </div>
         </div>
         {/**user button */}
-        <div className="relative h-full w-4/5 flex justify-center items-center">
-          <div className="bg-bermuda h-1/2 w-5/6 rounded-full flex justify-center items-center drop-shadow-xl p-3 dark:bg-dark-ternary">
-            <FontAwesomeIcon
-              icon={faUser}
-              className="w-1/6 font-medium p-1 dark:text-white"
-            />
-            <h6 className="text-black text-center w-4/6  dark:text-white">
-              {user ? (
-                <div className="w-full text-xs font-bold">{user.email}</div>
-              ) : (
-                <div>No user</div>
-              )}
+        <div className="h-full w-4/5 flex justify-center items-center">
+          <div className="flex items-center justify-center h-full w-3/4 ">
+            <h6 className="flex items-center justify-end text-ternary-blue text-[9px] sm:text-[10px] md:text-[12px] lg:text-[14px] font-inter w-4/6  dark:text-white">
+              {timeOfDay}&nbsp;{user !== null ? adminData.name : "no-user"}
             </h6>
-            <button
-              onClick={() => setIsOpen((prev) => !prev)}
-              className="p-4 w-1/6 flex items-center justify-between active:text-white"
-            >
-              {!isOpen ? (
-                <FontAwesomeIcon
-                  icon={faCaretDown}
-                  className="dark:text-white"
-                />
-              ) : (
-                <FontAwesomeIcon icon={faCaretUp} className="dark:text-white" />
-              )}
-            </button>
           </div>
-          {isOpen && (
-            <div className="bg-white absolute top-20 flex flex-col items-start rounded-lg p-5 w-full drop-shadow-xl ">
-              <div className="flex flex-col">
-                <div className="h-1/2 w-full ">
-                  <button className="text-red" onClick={logout}>
-                    Logout
-                  </button>
-                </div>
-              </div>
+          <div className="flex justify-center items-center md:border-l-2 lg:border-l-4 border-ternary-blue dark:border-ternary-blue Sh-full w-1/4 ">
+            <div className="flex items-center justify-center bg-white shadow-lg border-2  border-ternary-blue h-[30px] w-[30px]  sm:h-[37px] sm:w-[37px] md:h-[41px] md:w-[41px] lg:h-[45px] lg:w-[45px] rounded-full dark:border-silver">
+              <img
+                className="h-[28px] w-[28px] sm:h-[35px] sm:w-[35px] md:h-[39px] md:w-[39px] lg:h-[43px] lg:w-[43px] rounded-full"
+                src={
+                  adminData.profilePicture
+                    ? `${adminData.profilePicture}`
+                    : "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=600"
+                }
+                alt="userProPic"
+              />
             </div>
-          )}
+          </div>
         </div>
       </div>
     </>
